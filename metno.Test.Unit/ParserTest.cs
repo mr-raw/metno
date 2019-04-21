efa71bff0b427fbd9f0dab90e27cb11a2ebdf4f4
Erik Raae <erik@raae.eu>
2019-04-21 16:36:43 +0200

    Added a test project

---
+++ metno.Test.Unit/ParserTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace metno.Test.Unit
+{
+    [TestClass]
+    public class ParserTest
+    {        
+        [TestMethod]
+        public void ParserReturnsCorrect()
+        {
+            var htmlToBeStripped = "<html>Test</html>";
+            var result = Parser.StripHtml(htmlToBeStripped);
+
+            Assert.IsTrue(result.Equals("Test"));
+        }
+    }
+}
